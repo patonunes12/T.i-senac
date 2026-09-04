c1919982f80416e2d5010c1c7db701c794aad23c
rafael nunes <rafamae137@gmail.com>
2026-09-04 09:08:36 -0300

    arrow function

--- Front end/script.js
+++ Front end/script.js
@@ -1,4 +1,8 @@
-var var rafafa?
-var 
-var 
-var 
+const botao = document.getElementById('botao-acao');
+const cores = ['#00ff88', '#ff007f', '#00d2ff', '#ffb703', '#7109b6'];
+let indice = 0;
+
+botao.addEventListener('click', () => {
+  document.body.style.backgroundColor = cores[indice];
+  indice = (indice + 1) % cores.length;
+});
