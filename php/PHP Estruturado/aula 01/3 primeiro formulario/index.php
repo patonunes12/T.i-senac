736b26dfb27c4fc62c475b6e76c7216d33f91bd2
rafael nunes <rafamae137@gmail.com>
2026-09-17 11:35:38 -0300

    PHP2

---
+++ php/PHP Estruturado/aula 01/3 primeiro formulario/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sdfghjk</title>
+</head>
+<body>
+
+    <form action="dados.php" method="POST">
+        <label for="nome">Nome:</label>
+        <input name="nome" id="nome" type="text" placeholder="ex: MARCOS"></input>
+
+        <label for="peso">Peso:</label>
+        <input name="peso" id="peso" type="number" placeholder="ex: 69.5" step="0.10"></input>
+
+
+        .
+
+
+    
+</body>
+</html>
