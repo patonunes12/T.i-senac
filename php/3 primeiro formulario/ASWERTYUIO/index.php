5dc667494be04b84aa179f8d2ab7cb0add36d477
rafael nunes <rafamae137@gmail.com>
2026-09-25 09:22:34 -0300

    Update index.php

--- php/3 primeiro formulario/ASWERTYUIO/index.php
+++ php/3 primeiro formulario/ASWERTYUIO/index.php
@@ -94,7 +94,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>akira</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./nova/style.css">
 </head>
 <body>
 
